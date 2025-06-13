26ee4388acbb7edb8301c68ff20f32a2d1b88024
grfsv <umequiqui@gmail.com>
2025-06-13 13:43:54 +0900

    [add] gitkeep

--- services/api/src/infrastructure/src/main/kotlin/Framework.kt
+++ services/api/src/infrastructure/src/main/kotlin/Framework.kt
@@ -1,12 +1,15 @@
 package com.streview
 
-import io.ktor.http.*
 import io.ktor.server.application.*
+import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 
 fun Application.configureFramework() {
     install(Koin) {
+        modules(module {
 
+
+        })
     }
     println("Koin is installed")
 }
