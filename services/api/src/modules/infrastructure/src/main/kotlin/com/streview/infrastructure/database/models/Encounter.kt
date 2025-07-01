f4eefa825f22ae8a995b2531030af12e0796d710
grfsv <umequiqui@gmail.com>
2025-07-01 09:43:47 +0900

    [fix] dateに設定すべきだったものがdatetimeに設定されていたのを修正

--- services/api/src/modules/infrastructure/src/main/kotlin/com/streview/infrastructure/database/models/Encounter.kt
+++ services/api/src/modules/infrastructure/src/main/kotlin/com/streview/infrastructure/database/models/Encounter.kt
@@ -1,11 +1,11 @@
 package com.streview.infrastructure.database.models
 
 import org.jetbrains.exposed.v1.core.Table
-import org.jetbrains.exposed.v1.datetime.datetime
+import org.jetbrains.exposed.v1.datetime.date
 
 object EncounterTable : Table() {
     val id = varchar("user_id", 255)
     val encounterId = varchar("encounter_id", 255)
-    val localDate = datetime("local_date")
+    val localDate = date("local_date")
     override val primaryKey = PrimaryKey(id, encounterId, localDate)
 }
