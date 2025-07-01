8eeb95bb1356d89b0c07cc5720e07c8f25209c33
unSerori <pd.ai.374005475@gmail.com>
2025-07-01 16:29:45 +0900

    [sry]テストの文言を世界共通言語から日本語に変更

--- services/api/src/modules/domain/src/test/kotlin/com/streview/domain/stores/StoreTest.kt
+++ services/api/src/modules/domain/src/test/kotlin/com/streview/domain/stores/StoreTest.kt
@@ -7,8 +7,8 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldHaveLength
 
 class StoreTest : BehaviorSpec({
-    Given("Store domain model"){ // CONTEXT: どういう状況で、
-        When("execute factory func"){ // CONTEXT: どういう操作をしたとき、
+    Given("Storeのdomain model"){ // CONTEXT: どういう状況で、
+        When("factoryメソッドで新しいStoreを作成する"){ // CONTEXT: どういう操作をしたとき、
             // CONTEXT: 準備
             val storeName: String = "かつお"
 
@@ -16,22 +16,22 @@ class StoreTest : BehaviorSpec({
             val store: Store = Store.factory(storeName)
 
             // CONTEXT: 確認
-            Then("setting value"){ // CONTEXT: こうなるはずだ、という結果
+            Then("指定した店舗名がせっていされている"){ // CONTEXT: こうなるはずだ、という結果
                 store.name.value shouldBe storeName
             }
-            Then("auto gen id"){
+            Then("36桁のIDが自動で生成されている"){
                 store.id.value shouldHaveLength 36
             }
         }
 
-        When("rebuild existing store"){
+        When("reconstructメソッドで既存（ここでは、このタイミングで作成した値）のStoreを再構築する"){
             val id: String = UUID.generate().value
             println(id)
             val name: String = "rebuild store name"
 
             val store = Store.reconstruct(id, name)
 
-            Then("setting value"){
+            Then("reconstructメソッドに与えた値が正しく設定されている"){
                 store.id.value shouldBe id
                 store.name.value shouldBe name
             }
