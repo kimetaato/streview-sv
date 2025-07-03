d2241b0c6af26382b4de902888eb498ddbe0cde3
grfsv <umequiqui@gmail.com>
2025-07-03 11:56:11 +0900

    [fix] 集約のサイズを縮小

---
+++ services/api/src/modules/domain/src/main/kotlin/com/streview/domain/encounters/Encounter.kt
@@ -0,0 +1,58 @@
+package com.streview.domain.encounters
+
+import com.streview.domain.commons.UserID
+import com.streview.domain.exceptions.DuplicateEncounterException
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+
+class Encounter private constructor(
+    val actorID: UserID,
+    val encounterDate: EncounterDate,
+    private val _encounterIDs: MutableList<UserID>,
+) {
+    val encounterIDs: List<UserID>
+        get() = _encounterIDs.toList()
+
+    companion object {
+        fun factory(actorID: UserID, encounterDate: EncounterDate): Encounter {
+           return  Encounter(
+               actorID,
+               encounterDate,
+               emptyList<UserID>().toMutableList(),
+           )
+        }
+
+        fun factory(userID: UserID, encounterDate: EncounterDate, encounterIDs: List<UserID>): Encounter {
+            return Encounter(
+                userID,
+               encounterDate,
+                encounterIDs.toMutableList(),
+            )
+        }
+    }
+
+    // すれ違いしたユーザーを追加する
+    fun add(encounterID: UserID): Encounter {
+        if (encounterID == actorID) {
+            return this
+        }
+
+        if (_encounterIDs.contains(encounterID)) {
+            throw DuplicateEncounterException("すでにすれ違っています。:${encounterDate}")
+        }
+
+        _encounterIDs.add(encounterID)
+        return this
+    }
+
+}
+
+@JvmInline
+value class EncounterDate(val value: LocalDate) {
+    init {
+        val today = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
+        require(value <= today) { "未来の日付です。" }
+    }
+}
--- services/api/src/modules/infrastructure/src/main/kotlin/com/streview/infrastructure/database/encounters/EncounterMapper.kt
+++ services/api/src/modules/infrastructure/src/main/kotlin/com/streview/infrastructure/database/encounters/EncounterMapper.kt
@@ -1,39 +1,38 @@
 package com.streview.infrastructure.database.encounters
 
 import com.streview.domain.commons.UserID
-import com.streview.domain.encounts.Encounter
-import com.streview.domain.encounts.EncounterRepository
+import com.streview.domain.encounters.Encounter
+import com.streview.domain.encounters.EncounterDate
+import com.streview.domain.encounters.EncounterRepository
 import com.streview.infrastructure.database.models.EncounterTable
-import kotlinx.datetime.LocalDate
 import org.jetbrains.exposed.v1.core.ResultRow
 import org.jetbrains.exposed.v1.core.statements.UpdateBuilder
 
 
-fun EncounterRepositoryImpl.toDomain(userID: UserID, list: List<ResultRow>): Encounter {
-    val encountersOfDay = mutableMapOf<LocalDate, MutableList<UserID>>()
-    
+fun EncounterRepositoryImpl.toDomain(list: List<ResultRow>): Encounter {
+    val encounterIDs = mutableListOf<UserID>()
+
+    val actorID = UserID(  list.first()[EncounterTable.id])
+    val encounterDate = EncounterDate( list.first()[EncounterTable.encounterDate])
+
     list.forEach { item ->
-            val date = item[EncounterTable.localDate]
             val encounterId = item[EncounterTable.encounterId]
-            encountersOfDay.getOrPut(date) { mutableListOf() }
-                .add(UserID(encounterId))
+            encounterIDs.add(UserID(encounterId))
     }
     
     return Encounter.factory(
-        userID,
-        encountersOfDay.mapValues { it.value.toList() }
+        actorID,
+        encounterDate,
+        encounterIDs.toList()
     )
 }
 
 fun EncounterRepository.toTable(encounter: Encounter): List<(UpdateBuilder<*>) -> Unit> {
-    // 各遭遇をそれぞれ個別のレコードとして作成
-    return encounter.encountersByDay.flatMap { (date, list) ->
-        list.map { userID ->
-            {
-                it[EncounterTable.id] = encounter.actorID.value
-                it[EncounterTable.localDate] = date
-                it[EncounterTable.encounterId] = userID.value
-            }
+    return encounter.encounterIDs.map { encounterID ->
+        {
+            it[EncounterTable.id] = encounter.actorID.value
+            it[EncounterTable.encounterDate] = encounter.encounterDate.value
+            it[EncounterTable.encounterId] = encounterID.value
         }
     }
 }
