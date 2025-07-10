b64fab02b7d46cba44d56172736f6e53259878e7
grfsv <umequiqui@gmail.com>
2025-07-10 18:37:08 +0900

    [add] Relayてーぼー

---
+++ services/api/src/modules/infrastructure/src/main/kotlin/com/streview/infrastructure/database/models/Relays.kt
@@ -0,0 +1,10 @@
+package com.streview.infrastructure.database.models
+
+import org.jetbrains.exposed.v1.core.Table
+
+object RelaysTable : Table() {
+    val userID = char("user_id", 28)
+    val reviewUUID = char("review_uuid", 40)
+    val isReReviewed = bool("is_re_reviewed")
+    override val primaryKey = PrimaryKey(userID, reviewUUID)
+}
