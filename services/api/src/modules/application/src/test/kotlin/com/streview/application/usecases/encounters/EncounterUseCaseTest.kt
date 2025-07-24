4a25180c4119c764fdc0a4e7a8739a8ff2c94580
grfsv <umequiqui@gmail.com>
2025-07-24 16:25:55 +0900

    [fix] テストケースの修正内容の誤り

--- services/api/src/modules/application/src/test/kotlin/com/streview/application/usecases/encounters/EncounterUseCaseTest.kt
+++ services/api/src/modules/application/src/test/kotlin/com/streview/application/usecases/encounters/EncounterUseCaseTest.kt
@@ -106,7 +106,7 @@ class EncounterUseCaseTest : FreeSpec({
                 val response = useCase.execute(request)
 
                 // 検証
-                response.encounterCount shouldBe encounterDates.size * encounterIDs.size // レスポンス成功してる？
+                response.result shouldBe true // レスポンス成功してる？
                 coVerify(exactly = encounterDates.size) { mockRepository.findByID(actorID, any()) } // 日付の数だけ
                 coVerify(exactly = encounterDates.size) { mockRepository.save(any()) } // 日付の数だけ
                 mockEncounterMap.values.forEach { mockEncounter ->
