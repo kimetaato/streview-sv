52948c9f34f4c88f07282e5027244a3d96a8436b
grfsv <umequiqui@gmail.com>
2025-09-02 17:21:20 +0900

    [refactor] 変数名の修正

--- services/api/src/modules/application/src/main/kotlin/com/streview/application/usecases/encounters/EncounterUseCase.kt
+++ services/api/src/modules/application/src/main/kotlin/com/streview/application/usecases/encounters/EncounterUseCase.kt
@@ -5,6 +5,7 @@ import com.streview.application.services.EncounterDecryptionService
 import com.streview.application.usecases.UseCase
 import com.streview.application.usecases.encounters.dto.EncounterRequest
 import com.streview.application.usecases.encounters.dto.EncounterResponse
+import com.streview.domain.commons.UserID
 import com.streview.domain.commons.event.EventBus
 import com.streview.domain.encounters.Encounter
 import com.streview.domain.encounters.EncounterRepository
@@ -12,31 +13,32 @@ import com.streview.domain.exceptions.DuplicateEncounterException
 import org.jetbrains.exposed.v1.r2dbc.transactions.suspendTransaction
 
 class EncounterUseCase(
-    val eR: EncounterRepository,
-    private val dS: EncounterDecryptionService
+    private val encounterRepository: EncounterRepository,
+    private val encounterDecryptionService: EncounterDecryptionService
 ) : UseCase<EncounterRequest, EncounterResponse> {
     override suspend fun execute(input: EncounterRequest): EncounterResponse =
         suspendTransaction {
             var encounterCount = 0
             // 日付ごとのエンティティで処理を行う
             input.encounters.forEach { encounterData ->
-                eR.findByUserIDAndEncounterDate(input.userID, encounterData.date).fold(
+                encounterRepository.findByUserIDAndEncounterDate(input.userID, encounterData.date).fold(
                     success = { existingEncounter ->
                         // 既存のEncounterがあれば使用、なければ新規作成
                         val encounter = existingEncounter ?: Encounter.create(input.userID, encounterData.date)
 
                         // すれ違いを追加
-                        encounterData.encounter.forEach { encryptedEncounterID ->
+                        encounterData.encounter.forEach { encounterID ->
                             // 暗号化されたencounterIDから実際のUserIDを抽出
-                            val actualUserID = dS.extractUserID(encryptedEncounterID)
+//                            val actualUserID = dS.extractUserID(encryptedEncounterID)
+                            val encounterID = UserID(encounterID)
                             try {
-                                encounter.add(actualUserID)
+                                encounter.add(encounterID)
                                 encounterCount++
                             } catch (_: DuplicateEncounterException) {}
                         }
 
                         // 永続化
-                        eR.save(encounter)
+                        encounterRepository.save(encounter)
 
                         // イベント発行
                         encounter.domainEvents.map { event ->
